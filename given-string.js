c415b8f88d3cc5ec2d7302bf6c84347d570be075
Jagjeet Saini <jagjeetsaini@Jagjeets-MacBook-Air.local>
2023-05-03 14:47:03 +0530

    second commit

--- given-string.js
+++ given-string.js
@@ -10,10 +10,9 @@ Test Case - "ankit"
 Output - false       */
 
 const str = "Hello my name is Ankit";
+const lookup = "ankit"
 
 //converting string to array and diving elements on basis of space
-
-
 
 const Arr = [];
 let Word = '';
@@ -31,4 +30,19 @@ if (Word !== '') {
   Arr.push(Word);
 }
 
-console.log(Arr);
+//console.log(Arr);
+// looping over Arr and checking if the given string(word) is present or not
+let found = false; // Initialize a variable to keep track of whether the lookup was found
+
+for (let i=0; i<Arr.length; i++){
+    if(Arr[i].toLowerCase() === lookup.toLowerCase()){
+        found = true; // Set the variable to true if the lookup is found
+        break;
+    }
+}
+
+if (found) { // Check the variable to determine whether the lookup was found or not
+    console.log(true)
+} else {
+    console.log(false)
+}
